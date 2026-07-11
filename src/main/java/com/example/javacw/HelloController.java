3e64257d61ce3a3715c6fa1e64725049d0eeb3ed
betzyy <kisandusandith@gmail.com>
2026-07-12 00:41:28 +0530

    creation of fxIDs and linking them in scenebuilder

--- src/main/java/com/example/javacw/HelloController.java
+++ src/main/java/com/example/javacw/HelloController.java
@@ -1,8 +1,46 @@
 package com.example.javacw;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
+     @FXML
+     private Button inventoryDashboardButton;
+     @FXML
+     private Button posCheckoutButton;
+     @FXML
+     private Button dealerSelectionButton;
+     @FXML
+     private Button auditLoButton;
+     @FXML
+     private Button addNewPartButton;
+     @FXML
+     private Button updateSelectedButton;
+     @FXML
+     private Button deleteSelectedButton;
+     @FXML
+     private Button lowStockThresholdSave;
+     @FXML
+     private TextField keyworkSearchField;
+     @FXML
+     private TextField minPriceField;
+     @FXML
+     private TextField maxPriceField;
+     @FXML
+     private TextField lowStockThreshold;
+     @FXML
+     private TableView inventoryTable;
+     @FXML
+     private Label totalnventoryValue;
+     @FXML
+     private Label partsCount;
+     @FXML
+     private VBox lowStockWarning;
+
+
 
 }
